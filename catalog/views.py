be46cdd030d1d2ae9dd91e4bfe188fcc5e4cd53d
Semen Oskolkov <oskolkovsemen@yandex.ru>
2023-01-20 17:57:14 +0600

    Files with first project in Django

---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+
+
+def contacts(request):
+    if request.method == 'POST':
+        print(request.POST.get('name'))
+        print(request.POST.get('phone'))
+        print(request.POST.get('message'))
+    return render(request, 'catalog/contacts.html')
